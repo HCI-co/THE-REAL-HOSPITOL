b08ac3f095f0478c04ff02e3cd9e78e45722b696
Sanvritti <71001033+sanvritti@users.noreply.github.com>
2020-11-20 21:24:07 +0530

    Commit to Master
    I have tried doing the changes that paula did in the new version.

--- Hospital/Assets/FPSLookAround.cs
+++ Hospital/Assets/FPSLookAround.cs
@@ -11,8 +11,6 @@ public class FPSLookAround : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
